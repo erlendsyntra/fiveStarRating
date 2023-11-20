55f12d77ca4fb4c9c24941cccf4c7b6d4ac2aab5
erlendsyntra <erlendgeerts@gmail.com>
2023-11-20 16:36:46 +0100

    with hover, border around stars, animation for transitions

--- src/components/FiveStarRating/index.jsx
+++ src/components/FiveStarRating/index.jsx
@@ -3,28 +3,34 @@ import clsx from "clsx";
 import "./style.scss";
 const FiveStarRating = ({ staramount = 5, value = 0, disabled = false }) => {
   const [currentStars, setCurrentStars] = useState(+value);
+  const [hover, setHover] = useState(null);
   const classes = [...Array(staramount)].map((star, index) => {
     const myclass = clsx({
       star: true,
-      "star--full": index + 1 <= currentStars,
+      "star--full": index + 1 <= currentStars || index + 1 <= hover,
       "star--disabled": disabled,
     });
     return myclass;
   });
-  const logMe = (e) => {
+  const register = (e) => {
     if (e.target.nodeName === "SPAN" && !disabled) {
       setCurrentStars(e.target.dataset.starnr);
     }
   };
   return (
-    <div onClick={logMe}>
+    <div onClick={register}>
       {[...Array(staramount)].map((star, index) => {
+        const starValue = index + 1;
         return (
-          <span
-            key={index + 1}
-            data-starnr={index + 1}
-            className={classes[index]}
-          ></span>
+          <div className="outside">
+            <span
+              key={starValue}
+              data-starnr={starValue}
+              className={classes[index]}
+              onMouseEnter={() => setHover(starValue)}
+              onMouseLeave={() => setHover(null)}
+            ></span>
+          </div>
         );
       })}
       <p>Your rating is {currentStars} stars.</p>
